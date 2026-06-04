373bcaf2fb544390b4143b2d176e39d20e28cd9a
jschxll <schall.jan01@gmail.com>
2026-06-04 10:54:58 +0200

    new post: thoughts on 2001: A Space Odyssey & added rss feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ import { evaluate } from "@mdx-js/mdx";
 import { renderToStaticMarkup } from "react-dom/server";
 import { DateTime } from "luxon";
 import * as runtime from "react/jsx-runtime";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
 export default async function (eleventyConfig) {
   eleventyConfig.addExtension("mdx", {
@@ -23,12 +24,34 @@ export default async function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj).toFormat(format);
   });
 
+  eleventyConfig.addCollection("thoughts", function(collectionApi) {
+    return collectionApi.getFilteredByTag("thoughts").reverse(); 
+  });
+
   eleventyConfig.addTemplateFormats("mdx");
   eleventyConfig.addPassthroughCopy("src/bundle.css");
   eleventyConfig.addPassthroughCopy("src/img/**/*");
   eleventyConfig.addPassthroughCopy("src/gallery/**/*");
   eleventyConfig.addPassthroughCopy("src/*.js");
   eleventyConfig.addPassthroughCopy("src/fonts/*");
+
+  eleventyConfig.addPlugin(feedPlugin, {
+		type: "rss",
+		outputPath: "/feed.xml",
+		collection: {
+			name: "thoughts",
+			limit: 0,
+		},
+		metadata: {
+			language: "en",
+			title: "Sights of My Life",
+			subtitle: "A RSS feed of my content:)",
+			base: "https://jschxll.net/",
+			author: {
+				name: "jschxll",
+			}
+		}
+	});
   
   return {
     dir: {
